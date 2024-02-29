ad38410bdae63e674f2ed8366b88d851621a7f1f
Augustin Sande Idohou <asidev7@gmail.com>
2024-02-29 09:10:11 +0100

    ajout

--- projet/settings.py
+++ projet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -270,6 +271,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 
 # Static files (CSS, JavaScript, Images)
